925677018c25bf4fe1d4d625e2a214fdcd3ca46a
rafaelrrs <you@example.cyou@example.com>
2021-05-11 23:43:28 -0300

    first commit

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CursoMVC;Integrated Security=True");
+        }
+        public void SetModified(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
